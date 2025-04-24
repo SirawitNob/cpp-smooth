f26c7b1dbf0289b0d27960acc65e2657cb1090ba
NobSirawit <nobsirawit@gmail.com>
2025-04-24 15:20:05 +0700

    feat: sample code of string concatination with length of source string in c++.

--- string/functions.cpp
+++ string/functions.cpp
@@ -9,13 +9,25 @@ int main(){
     cin.getline(p, 50);
     cout << strlen(p) << endl;
     
+    cout << "String concatination." << endl;
     char s1[20];
     char s2[20];
-    int len;
     cout << "Enter destination string : ";
     cin.getline(s1, 20);
     cout << "Enter source string : ";
     cin.getline(s2, 20);
     cout << "Concat with strcat(destination, source) : " << strcat(s1, s2) << endl;
+    
+    cout << "Another String concatination." << endl;
+    char v1[20];
+    char v2[20];
+    int len;
+    cout << "Enter destination string : ";
+    cin.getline(v1, 20);
+    cout << "Enter source string : ";
+    cin.getline(v2, 20);
+    cout << "Enter length of source string to be concat : ";
+    cin >> len;
+    cout << "Concat with strncat(destination, source, length) : " << strncat(v1, v2, len) << endl;
     return 0;
 }
