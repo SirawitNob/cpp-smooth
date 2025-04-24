de090f677d8742267a308b12167be3439310c957
NobSirawit <nobsirawit@gmail.com>
2025-04-24 15:12:18 +0700

    feat: sample code of string concatination function in c++.

--- string/functions.cpp
+++ string/functions.cpp
@@ -3,9 +3,19 @@
 using namespace std;
 
 int main(){
-    char p[20];
+    cout << "Find length of string." << endl;
+    char p[50];
     cout << "Enter String : ";
-    cin.getline(p, 20);
+    cin.getline(p, 50);
     cout << strlen(p) << endl;
+    
+    char s1[20];
+    char s2[20];
+    int len;
+    cout << "Enter destination string : ";
+    cin.getline(s1, 20);
+    cout << "Enter source string : ";
+    cin.getline(s2, 20);
+    cout << "Concat with strcat(destination, source) : " << strcat(s1, s2) << endl;
     return 0;
 }
